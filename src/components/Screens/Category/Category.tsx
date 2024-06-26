b627efd8024c06114da69a753a3cba41cec8c443
juan-soares <juan.css013@gmail.com>
2024-06-26 14:19:54 -0300

    category

--- src/components/Screens/Category/Category.tsx
+++ src/components/Screens/Category/Category.tsx
@@ -1,51 +1,33 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ICardInfo, ICategory } from "../../../interfaces";
-import { CardsList } from "./CardsList";
-import { Filters } from "./Filters";
+import { ICategory } from "../../../interfaces";
 
 export function ScreenCategory() {
   const { categoryURL } = useParams();
-  const [category, setCategory] = useState<null | ICategory>(null);
-  const [cardsInfo, setCardsInfo] = useState<ICardInfo[]>([]);
-  
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch(
-        `https://catalog-1kpk--3001--802dc1bc.local-credentialless.webcontainer.io/categories?url=${categoryURL}`
-      );
+  const [categoryObject, setCategoryObject] = useState<ICategory | null>(null);
 
-      const data = await res.json();
-
-      setCategory(data[0]);
-
-      const resList = await fetch(
-        `https://catalog-1kpk--3001--802dc1bc.local-credentialless.webcontainer.io/${data[0].collection}`
-      );
-
-      const dataList = await resList.json();
-
-      setCardsInfo(dataList);
-    };
-
-    getData();
-  }, [categoryURL]);
-
-  if (!category) {
-    return (
-      <div>
-        <p>Carregando...</p>
-      </div>
+  const getCategoryObject = async (categoryURL: string) => {
+    const res = await fetch(
+      `https://catalog-1kpk--3001--dc4d7514.local-credentialless.webcontainer.io/categories?url=${categoryURL}`
     );
-  } else {
-    const { titleBR, filters } = category;
 
+    const data = await res.json();
+
+    setCategoryObject(data[0]);
+  };
+
+  useEffect(() => {
+    getCategoryObject(categoryURL || "");
+  }, []);
+
+  if (!categoryObject) {
+    return <div>Carregando...</div>;
+  } else {
+    const { titleBR } = categoryObject;
 
     return (
       <div>
         <h1>{titleBR}</h1>
-        <Filters filters={filters} />
-        <CardsList cardsInfo={cardsInfo} />
       </div>
     );
   }
